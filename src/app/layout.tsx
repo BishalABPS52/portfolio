14e48defe9bc2f96a4b696d69e9d1a25cd24518d
Bishal Shrestha <167290532+BishalABPS52@users.noreply.github.com>
2025-06-19 19:19:25 +0545

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: "Bishal Shrestha - Developer & Designer",
+  title: "Bishal Shrestha - Developer & Software Engineering Enthusiast",
   description:
     "Developer",
   icons: {
@@ -59,7 +59,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Bishal Shrestha - Developer & Designer",
+    title: "Bishal Shrestha - Developer & Software Engineering Enthusiast",
     description: "Developer",
     creator: "@BishalABPS52",
   },
